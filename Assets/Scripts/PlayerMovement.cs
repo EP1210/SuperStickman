beb928ad3951c0c26dbc76832677a33fed157700
Lukas Brezina <lukas.brezina@stud.fh-campuswien.ac.at>
2023-12-28 14:55:37 +0100

    PlayerMovement (partly) and Camera

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private new Camera camera;
+    private new Rigidbody2D rigidbody;
+    private Vector2 velocity;
+    private float inputAxis;
+  
+    public float movementSpeed = 8f;
+   
+    private void Awake() 
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() 
     {
-        
+        HorizontalMovement();
+    }
+
+    private void HorizontalMovement()  
+    {
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * movementSpeed, movementSpeed * Time.deltaTime);
+    }
+
+    private void FixedUpdate() 
+    {
+        Vector2 position = rigidbody.position;
+        position += velocity * Time.fixedDeltaTime;
+
+        Vector2 leftEdge = camera.ScreenToWorldPoint(Vector2.zero);
+        Vector2 rightEdge = camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        position.x = Mathf.Clamp(position.x, leftEdge.x, rightEdge.x);
+
+        rigidbody.MovePosition(position);
     }
 }
