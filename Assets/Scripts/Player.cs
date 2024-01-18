8c2876737387651158587c33566abda8ecac0625
Lukas110596 <schertlerlukas96@gmail.com>
2024-01-18 13:57:40 +0100

    Implementet Hearts and logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,41 +1,71 @@
-
-using UnityEditor.Build.Content;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     private DeathAnimation deathAnimation;
-    private int initialHealth = 3;
-    private int currentHealth = 0;
+    private int maxHealth = 3;
+    private int currentHealth;
     public int score = 0;
 
-     void Start(){
-        currentHealth = initialHealth; 
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHeartsUI();
     }
 
-    private void Awake(){
+    private void Awake()
+    {
         deathAnimation = GetComponent<DeathAnimation>();
     }
 
-  public void Hit(){
-    if (currentHealth >= 1){
-        currentHealth -= 1;
-    } else {
-        Death();
+    public void Hit()
+    {
+        if (currentHealth > 0)
+        {
+            currentHealth -= 1;
+            UpdateHeartsUI(); // Update the UI when the player is hit
+
+            if (currentHealth == 0)
+            {
+                Death();
+            }
+        }
     }
 
-  }
-
-    public void Death(){
+    public void Death()
+    {
         deathAnimation.enabled = true;
         GameManager.Instance.ResetLevel(3f);
     }
 
-    private void OnCollisionEnter2D (Collision2D collision) {
-        if (collision.gameObject.CompareTag("Water")) {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Water"))
+        {
             Death();
         }
     }
 
-  
+    // Update the UI to reflect the current health
+    private void UpdateHeartsUI()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if (i < currentHealth)
+            {
+                // Display full hearts for remaining health
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                // Display empty hearts for lost health
+                hearts[i].sprite = emptyHeart;
+            }
+        }
+    }
 }
