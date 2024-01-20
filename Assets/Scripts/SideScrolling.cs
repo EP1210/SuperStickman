57fab42b1ebbd50b93782ff27945ecbef02ba79c
Alex Messner :) <alexander.messner1@stud.fh-campuswien.ac.at>
2024-01-20 19:42:13 +0100

    Sterben/respawnen & Sounds funktioniert auf jedem level

--- Assets/Scripts/SideScrolling.cs
+++ Assets/Scripts/SideScrolling.cs
@@ -4,16 +4,15 @@ public class SideScrolling : MonoBehaviour
 {
     private Transform player;
 
-    private void Awake()
-    {
-        player = GameObject.FindWithTag("Player").transform;
-    }
+    
 
     // make the camera to follow the player and centralize player while running right
     private void LateUpdate()
     {
+        player = GameObject.FindWithTag("Player").transform;
         Vector3 cameraPosition = transform.position;
-        cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        //cameraPosition.x = Mathf.Max(cameraPosition.x, player.position.x);
+        cameraPosition.x = player.position.x;
         transform.position = cameraPosition;
     }
 }
